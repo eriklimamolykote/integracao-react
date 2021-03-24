b96528ce57b0105dce83211c5a6452ff111ee9a6
Erik Lima <eriklim@hotmail.com>
2021-03-24 16:57:46 -0300

    segundo-commit-integracao-react-24-03-2021

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, { Component } from 'react';
 import './App.css';
+import Contador from './componentes/ContadorValor'
 
 function App() {
   return (
     <div className="App">
-      
+      <Contador valorInicial={9876} />
+      <Contador />
     </div>
   );
 }
